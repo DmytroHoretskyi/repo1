494ebe3bc8578f3e083f5fb6593b1d3ad2b7ec99
DmytroHoretskyi <serwserw16@gmail.com>
2020-11-23 09:57:43 +0300

    my first commit

---
+++ Source231.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+char third_laba(char A[]) {
+  
+
+    for (int i = 1; i < strlen(A); ++i) {
+        if (A[i] + 1 != A[i -1])  
+            return A[i] + 1; 
+
+    }
+
+    return 0;
+}
+
+int main() {
+    char A[100];
+    printf("\nEnter 5 letters");
+    gets(A);
+    printf("%s", A);
+
+    for (int i = 0; i <= strlen(A); i++) {
+        if (A[i] >= 65 && A[i] <= 90)
+            A[i] = A[i] + 32;
+    }
+
+    char missResult = third_laba(A);
+    printf("\n");
+    printf("%c", missResult);
+
+    return 0;
+}
